2ac48556bd589061516d22c10dc6d50ca781ac72
loaiahmed <loaiahmedsalem9@gmail.com>
2026-03-04 17:02:57 +0200

    fix session secure cookie

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: { contentSecurityPolicy: false },
+  },
+  {
+    name: 'strapi::session',
+    config: { secure: false },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
