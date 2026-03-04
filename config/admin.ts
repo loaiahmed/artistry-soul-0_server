ab078135a1482861248818931bcd2e8de078df16
loaiahmed <loaiahmedsalem9@gmail.com>
2026-03-04 17:20:48 +0200

    enough

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,13 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      enabled: true,
+      cookieOptions: {
+        secure: false,
+        sameSite: 'lax',
+      },
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
