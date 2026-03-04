597b8685523f4ac8bd472bb76acbf8a06d61a677
loaiahmed <loaiahmedsalem9@gmail.com>
2026-03-04 17:32:19 +0200

    fixing secure cookie bug

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-    sessions: {
-      enabled: true,
-      cookieOptions: {
-        secure: false,
-        sameSite: 'lax',
-      },
-    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
